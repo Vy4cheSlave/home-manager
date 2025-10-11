8edf7a9ea41c1cbcb2c836c4d56339120e067e30
['kay] <115963849+Vy4cheSlave@users.noreply.github.com>
2025-10-11 21:17:09 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in {
 
-      systemConfigs.default = inputs.system-manager.lib.makeSystemConfig {
+      systemConfigs.${system}.default = inputs.system-manager.lib.makeSystemConfig {
         modules = [
           inputs.nix-system-graphics.systemModules.default
           ({
