4f39fd0de381cb2ce04ffeaf8ef7427e1efdb648
['kay] <115963849+Vy4cheSlave@users.noreply.github.com>
2025-10-11 20:09:20 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,18 +8,53 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # INFO NOT TESTED
+    system-manager = {
+      url = "github:numtide/system-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-system-graphics = {
+      url = "github:soupglasses/nix-system-graphics";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, ... }@inputs:
     let 
+      username = "vch";
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system;};
+      pkgs = import nixpkgs { system = "${username}"; config.allowUnfree = true; };
     in {
+
+      systemConfigs.default = inputs.system-manager.lib.makeSystemConfig {
+        modules = [
+          inputs.nix-system-graphics.systemModules.default
+          ({
+            config = {
+              nixpkgs.hostPlatform = "${system}";
+              inputs.system-manager.allowAnyDistro = true;
+              system-graphics.enable = true;
+            };
+          })
+        ];
+      };
+
+      devShells."${system}".default = pkgs.mkShellNoCC {
+        packages = [
+          inputs.system-manager.packages."${system}".default
+        ];
+      };
+
       homeConfigurations = {
-        vch = home-manager.lib.homeManagerConfiguration {
+        "${username}" = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
-          modules = [ ./home.nix ];
+          modules = [ 
+            ./home.nix 
+            ({
+              home.packages = [ inputs.system-manager.packages."${system}".default ];
+            })
+          ];
         };
       };
     };
