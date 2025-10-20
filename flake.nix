fc3ebb8f0918cbb2a1e8e7de51689d06aa09c415
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-20 21:04:18 +0700

    cleaning

--- flake.nix
+++ flake.nix
@@ -8,15 +8,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # INFO NOT TESTED
-    #system-manager = {
-    #  url = "github:numtide/system-manager";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-    #nix-system-graphics = {
-    #  url = "github:soupglasses/nix-system-graphics";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
   };
 
   outputs = { nixpkgs, ... }@inputs:
@@ -26,34 +17,12 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in {
-
-      #systemConfigs.default = inputs.system-manager.lib.makeSystemConfig {
-      #  modules = [
-      #    inputs.nix-system-graphics.systemModules.default
-      #    ({
-      #      config = {
-      #        nixpkgs.hostPlatform = "${system}";
-      #        system-manager.allowAnyDistro = true;
-      #        system-graphics.enable = true;
-      #      };
-      #    })
-      #  ];
-      #};
-
-      #devShells."${system}".default = pkgs.mkShellNoCC {
-      #  packages = [
-      #    inputs.system-manager.packages."${system}".default
-      #  ];
-      #};
-
-      #homeConfigurations = {
-        #"${username}" = inputs.home-manager.lib.homeManagerConfiguration {
       nixosConfigurations = {
-        "${username}" = nixpkgs.lib.nixosSystem { 
+        "${username}" = nixpkgs.lib.nixosSystem 
+        { 
           system = "${system}"; 
           inherit pkgs;
           modules = [ 
-            #./home.nix 
             ./configuration.nix
             inputs.home-manager.nixosModules.home-manager {
               home-manager = {
@@ -63,9 +32,6 @@
                 backupFileExtension = "backup";
               };
             }
-            #({
-            #  home.packages = [ inputs.system-manager.packages."${system}".default ];
-            #})
           ];
         };
       };
