e48293667d574c5f2668bc9d200c67c7152c8535
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-23 23:25:33 +0700

    -(%d-%A) %H:%M')

--- web-browser/librefox.nix
+++ web-browser/librefox.nix
@@ -81,7 +81,7 @@
           "identity.fxaccounts.enabled" = { Value = true; Status = "locked"; };  # Включение синхронизации
           "browser.startup.page" = { Value = 3; Status = "locked"; }; # 1: новые вкладки, 2: домашняя страница, 3: предыдущие вкладки
           "browser.sessionstore.resume_from_crash" = lock-true; # Восстанавливать предыдущие вкладки после сбоя
-          "browser.sessionstore.resume_session" = lock-true;; # Восстанавливать предыдущую сессию, если Firefox был закрыт 
+          "browser.sessionstore.resume_session" = lock-true; # Восстанавливать предыдущую сессию, если Firefox был закрыт 
         #  "browser.contentblocking.category" = { Value = "standard"; Status = "locked"; }; # been set "strict"
         #  "extensions.pocket.enabled" = lock-false;
         #  "extensions.screenshots.disabled" = lock-true;
