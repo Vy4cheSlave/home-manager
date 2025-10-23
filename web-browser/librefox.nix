c9f9a8535633e66556490d91fa2357ff89063487
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-24 00:08:22 +0700

    -(%d-%A) %H:%M')

--- web-browser/librefox.nix
+++ web-browser/librefox.nix
@@ -79,7 +79,7 @@
           "ui.systemUsesDarkTheme" = lock-true;
           "sidebar.animation.enabled" = lock-false;
           "sidebar.main.tools" = { Value = "history,syncedtabs,bookmarks"; Status = "locked"; }
-          "sidebar.position_start" = lock-true;
+          "sidebar.position_start"; # = lock-true;
           "sidebar.revamp" = lock-true;
           "sidebar.visibility" = { Value = "hide-sidebar"; Status = "locked"; };
           "sidebar.verticalTabs" = lock-true;
