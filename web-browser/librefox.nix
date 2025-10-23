a01352f525347dcc99a65eb7f9aa930a23fd48c3
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-23 23:04:52 +0700

    -(%d-%A) %H:%M')

--- web-browser/librefox.nix
+++ web-browser/librefox.nix
@@ -76,9 +76,9 @@
         # Check about:config for options.
         Preferences = {
           "ui.systemUsesDarkTheme" = { Value = 3; Status = "locked"; }; # 1 = включена темная тема
-        #  "sidebar.verticalTabs" = { Value = false; Status = "locked"; };
-        #  "identity.fxaccounts.enabled" = { Value = true; Status = "locked"; };  # Включение синхронизации
-        #  "browser.startup.page" = { Value = 3; Status = "locked"; }; # 1: новые вкладки, 2: домашняя страница, 3: предыдущие вкладки
+          "sidebar.verticalTabs" = { Value = false; Status = "locked"; };
+          "identity.fxaccounts.enabled" = { Value = true; Status = "locked"; };  # Включение синхронизации
+          "browser.startup.page" = { Value = 3; Status = "locked"; }; # 1: новые вкладки, 2: домашняя страница, 3: предыдущие вкладки
           "browser.sessionstore.resume_from_crash" = { Value = true; Status = "locked"; }; # Восстанавливать предыдущие вкладки после сбоя
           "browser.sessionstore.resume_session" = { Value = true; Status = "locked"; }; # Восстанавливать предыдущую сессию, если Firefox был закрыт 
         #  "browser.contentblocking.category" = { Value = "standard"; Status = "locked"; }; # been set "strict"
