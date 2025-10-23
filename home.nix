352507e3970354c36ae80c5b0e7967b349ed8d59
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-24 05:22:52 +0700

    2025-[10-октября]-[23-Четверг] 22:22

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
       # fonts #################
       jetbrains-mono 
       nerd-fonts.jetbrains-mono
-      nerd-fonts.nerd-fonts-agave
+      nerd-fonts.agave
       nerd-fonts._3270
       #########################
       ghostty
