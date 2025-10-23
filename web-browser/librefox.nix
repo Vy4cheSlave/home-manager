b47a54d38475f2269ab9cbfed1f91723942acc75
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-23 23:23:51 +0700

    -(%d-%A) %H:%M')

--- web-browser/librefox.nix
+++ web-browser/librefox.nix
@@ -75,13 +75,13 @@
         /* ---- PREFERENCES ---- */
         # Check about:config for options.
         Preferences = {
-          "ui.systemUsesDarkTheme" = { Value = 3; Status = "locked"; }; # 1 = включена темная тема
+          "ui.systemUsesDarkTheme" = { Value = 3; Status = "unlocked"; }; # 1 = включена темная тема
           "sidebar.verticalTabs" = lock-true;
           "sidebar.verticalTabs.dragToPinPromo.dismissed" = lock-true;
           "identity.fxaccounts.enabled" = { Value = true; Status = "locked"; };  # Включение синхронизации
           "browser.startup.page" = { Value = 3; Status = "locked"; }; # 1: новые вкладки, 2: домашняя страница, 3: предыдущие вкладки
-          "browser.sessionstore.resume_from_crash" = { Value = true; Status = "locked"; }; # Восстанавливать предыдущие вкладки после сбоя
-          "browser.sessionstore.resume_session" = { Value = true; Status = "locked"; }; # Восстанавливать предыдущую сессию, если Firefox был закрыт 
+          "browser.sessionstore.resume_from_crash" = lock-true; # Восстанавливать предыдущие вкладки после сбоя
+          "browser.sessionstore.resume_session" = lock-true;; # Восстанавливать предыдущую сессию, если Firefox был закрыт 
         #  "browser.contentblocking.category" = { Value = "standard"; Status = "locked"; }; # been set "strict"
         #  "extensions.pocket.enabled" = lock-false;
         #  "extensions.screenshots.disabled" = lock-true;
