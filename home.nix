927e83664ff901802205c3faceb5169f3ed4138e
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-25 17:45:35 +0700

    2025-[10-октября]-[25-Суббота] 10:45

--- home.nix
+++ home.nix
@@ -88,7 +88,7 @@
   #####################################################################################################
   # flatpak############################################################################################
   home.sessionVariables = {
-    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
+    XDG_DATA_DIRS = lib.mkForce  "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
   };
   #####################################################################################################
 }
