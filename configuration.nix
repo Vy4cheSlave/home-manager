1720138849257c7959de9f221060ab859903e5ed
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-25 21:06:55 +0700

    2025-[10-октября]-[25-Суббота] 14:06

--- configuration.nix
+++ configuration.nix
@@ -109,38 +109,43 @@
   services.blueman.enable = true;
 ####################################################################
 ### Flatpak ########################################################
-  # services.flatpak.enable = true;
-  # systemd.services.flatpak-repo = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   path = [ pkgs.flatpak ];
-  #   script = ''
-  #     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  #   '';
-  # };
-  # test ################
-  # services.flatpak = {
-  #   enable = true;
-  #   packages = [
-  #     "md.obsidian.Obsidian"
-  #     "com.usebottles.bottles"
-  #   ];
-  # };
-  # test 2 ##############
-  services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "md.obsidian.Obsidian"
+      "com.usebottles.bottles"
+    ];
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly"; # Default value
+    };
+    services.flatpak.overrides = {
+      global = {
+        # Force Wayland by default
+        Context.sockets = ["wayland" "!x11" "!fallback-x11"];
+
+        Environment = {
+          # Fix un-themed cursor in some Wayland apps
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+          # Force correct theme for some GTK apps
+          GTK_THEME = "Adwaita:dark";
+        };
+      };
+
+      # "com.visualstudio.code".Context = {
+      #   filesystems = [
+      #     "xdg-config/git:ro" # Expose user Git config
+      #     "/run/current-system/sw/bin:ro" # Expose NixOS managed software
+      #   ];
+      #   sockets = [
+      #     "gpg-agent" # Expose GPG agent
+      #     "pcsc" # Expose smart cards (i.e. YubiKey)
+      #   ];
+      # };
+      "org.onlyoffice.desktopeditors".Context.sockets = ["x11"]; # No Wayland support
+    };
   };
-  #######################
-  # environment.variables = {
-  #   XDG_DATA_DIRS = lib.mkDefault [
-  #     "/var/lib/flatpak/exports/share"
-  #     "${config.users.users.vch.home}/.local/share/flatpak/exports/share"
-  #   ];
-  # };
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
