79c24d581602d867ba575e0efacdb881e865afcd
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-25 17:35:39 +0700

    2025-[10-октября]-[25-Суббота] 10:35

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,16 @@
   };
   services.blueman.enable = true;
 ####################################################################
+### Flatpak ########################################################
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+####################################################################
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
