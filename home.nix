98481a97877c826836768fb44b553c1f181ec4ee
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-27 06:03:17 +0700

    2025-[10-октября]-[26-Воскресенье] 23:03

--- home.nix
+++ home.nix
@@ -44,10 +44,11 @@
     stateVersion = "25.05";
   };
 
+  # xdg.configFile."btop/btop.conf".source = .conf/btop/btop.conf;
   programs.btop = {
     enable = true;
-    themes = .conf/btop/themes
-    xdg.configFile."btop/btop.conf".source = .conf/btop/btop.conf;
+    themes = .conf/btop/themes;
+    settings = .conf/btop;
   };
   programs.yazi = {
     enable = true;
