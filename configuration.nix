367c3b73e6aa73b8db27b554011ae944684f7917
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-26 20:13:16 +0700

    2025-[10-октября]-[26-Воскресенье] 13:13

--- configuration.nix
+++ configuration.nix
@@ -115,7 +115,7 @@ programs.steam = {
   # settings->general->launch options->
   # gamemoderun %command% / gamescope %command%
   enable = true;
-  # gamescopeSession.enable = true; # проблемы с масштабированием
+  gamescopeSession.enable = false; # проблемы с масштабированием
   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
