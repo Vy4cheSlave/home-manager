70b650dabc7dd5a4dffdaea5348deb2cfc584317
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-26 14:38:24 +0700

    2025-[10-октября]-[26-Воскресенье] 07:38

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@
           "~/.nix-profile/bin:ro"
           "xdg-config/git:ro"
         ];
-        Environment = [ "PATH=/usr/bin:/bin:/app/bin:/var/run/host/usr/bin:/home/yourusername/.nix-profile/bin" ];
+        Environment = [ "PATH=/usr/bin:/bin:/app/bin:/var/run/host/usr/bin:/home/vch/.nix-profile/bin" ];
       };
       "com.usebottles.bottles".Context = {
         # Явно указываем, что Bottles должен использовать NVIDIA GPU
