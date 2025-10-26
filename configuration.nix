02a14bb2ceff32d0ea8f93d98027dce9b70526cf
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-26 14:55:07 +0700

    2025-[10-октября]-[26-Воскресенье] 07:55

--- configuration.nix
+++ configuration.nix
@@ -136,9 +136,8 @@
 
       "md.obsidian.Obsidian".Context = {
         filesystems = [
-          # "/run/current-system/sw/bin:ro"
-          "~/.nix-profile/bin:ro"
           "xdg-config/git:ro"
+          "/run/current-system/sw/bin:ro"
         ];
         Environment = [ "PATH=/usr/bin:/bin:/app/bin:/var/run/host/usr/bin:/home/vch/.nix-profile/bin" ];
       };
