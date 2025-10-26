3497cbebb41bc1fe9b256a107c3e44c5f74e6968
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-26 19:28:03 +0700

    2025-[10-октября]-[26-Воскресенье] 12:28

--- configuration.nix
+++ configuration.nix
@@ -109,12 +109,12 @@
   services.blueman.enable = true;
 ####################################################################
 ### Steam ##########################################################
+programs.gamemode.enable = true; # улучшение производительности
 programs.steam = {
   # settings->general->launch options->
   # gamemoderun %command% / gamescope %command%
   enable = true;
   gamescopeSession.enable = true; # проблемы с масштабированием
-  gamemode.enable = true; # улучшение производительности
   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
