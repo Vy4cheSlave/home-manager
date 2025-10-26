4b2b08a67b2a61e4614fc73d4839a88844d0d052
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-27 06:17:10 +0700

    2025-[10-октября]-[26-Воскресенье] 23:17

--- home.nix
+++ home.nix
@@ -89,7 +89,7 @@
   targets.genericLinux.enable = true; # ENABLE THIS ON NON NIXOS
   programs.home-manager.enable = true;
   # niri ##############################################################################################
-  xdg.configFile."niri/config.kdl".source = .conf/wm/niri/config.kdl;
+  xdg.configFile."niri/config.kdl".source = ./conf/wm/niri/config.kdl;
 
   programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
