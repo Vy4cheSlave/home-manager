ec64a2f967b9cc9d5d8ddfbb1abd9c85c812f1d1
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-10-27 18:36:59 +0700

    2025-[10-октября]-[27-Понедельник] 11:36

--- home.nix
+++ home.nix
@@ -25,9 +25,6 @@
       # zapret
       # nftables      
 
-      # niri ##############
-      swaybg
-      #####################
       # eww ###############
       libnotify
       jq
@@ -90,7 +87,6 @@
 
   programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
-  programs.waybar.enable = true; # launch on startup in the default setting (bar)
   services.mako.enable = true; # notification daemon
   services.swayidle.enable = true; # idle management daemon
   services.polkit-gnome.enable = true; # polkit
